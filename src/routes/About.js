aa13e8143ef86bd6f063ee2bf6f02b4c8ae1a644
potter9733 <potter9733@users.noreply.github.com>
2021-01-23 03:49:55 +0900

    router

---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import { func } from "prop-types";
+import React from "react";
+
+function About() {
+  return <span>About thes page: I bulit it because I love movies.</span>;
+}
+
+export default About;
